bab03ab31a2b787572d955ad492a83e604b644ba
dikaa-bit <DIKARSKII90@GMAIL.COM>
2025-05-26 21:46:46 +0700

    Add files via upload

---
+++ tugas7/list.py
@@ -0,0 +1,22 @@
+#buat list
+# mylist = [5,7,3]
+# mylist2 = (('a','b','c'))
+
+#urutan data
+# print('Data :',mylist)
+# print('Data :',mylist2)
+# print('------------------')
+
+#data ganda
+# mylist = [5,7,3,3]
+# mylist2 = (('a','b','c','c'))
+# print('Data :',mylist)
+# print('Data :',mylist2)
+# print('------------------')
+
+#index
+# myIndex = mylist.index(7)
+# print('Index angka 7:',myIndex)
+# print('----------------------')
+
+#change
