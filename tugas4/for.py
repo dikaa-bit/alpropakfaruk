fbf2cf3e4a404e51976ed5da81b299a2ee869798
dikaa-bit <DIKARSKII90@GMAIL.COM>
2025-05-26 21:57:52 +0700

    Add files via upload

---
+++ tugas4/for.py
@@ -0,0 +1,12 @@
+# contoh 1
+print('FOR IN SEQUENCE')
+nama = ['Alfy','Alif','Danu','Kiki','Maira']
+for data in nama :
+    print('Nama :',data)
+print('------------')
+
+# contoh 2
+print('FOR IN RANGE')
+for nilai in range(1,11) :
+    print('Pengulangan ke : ',nilai)
+print('-----------')
